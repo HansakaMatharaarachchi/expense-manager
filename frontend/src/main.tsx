164ab2e90cd46d833038d885c8b3201995e22d5b
Hansaka Matharaarachchi <m.a.hansaka@gmail.com>
2025-03-26 16:34:46 +0530

    setup frontend routing

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import routes from "./routes";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<BrowserRouter>
+			<Routes>
+				{routes.map((route, index) => (
+					<Route key={index} path={route.path} element={route.element} />
+				))}
+			</Routes>
+		</BrowserRouter>
+	</StrictMode>
+);
